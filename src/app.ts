ca519329bee01551b69d4598ebe42bb6ced0658d
Naoto31 <naoto0910sai@gmail.com>
2021-05-13 18:59:15 +0200

    send static file to client side

--- src/app.ts
+++ src/app.ts
@@ -7,5 +7,7 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
+app.use("/src", express.static('./src/'));
+
 app.listen(4200, () => console.log('Server running'))
 
